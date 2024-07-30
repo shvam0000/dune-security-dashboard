30ea0dfa8f9b5f4632bd1c0ba145c21750e087ab
shvam0000 <shvam0000@gmail.com>
2024-07-30 10:48:37 +0530

    chore: add data file

---
+++ src/data/Dashboard_Dune_Security.js
@@ -0,0 +1,169 @@
+export const dashboardData = {
+  risk_score: 17.404411764705884,
+  score_change: -0.05,
+  risk_score_over_time: [
+    {
+      timestamp: '2024-05-01T12:00:00',
+      risk_score: 20.382930513595166,
+    },
+    {
+      timestamp: '2024-05-02T12:00:00',
+      risk_score: 20.382930513595166,
+    },
+    {
+      timestamp: '2024-05-03T12:00:00',
+      risk_score: 20.382930513595166,
+    },
+    {
+      timestamp: '2024-05-04T12:00:00',
+      risk_score: 20.382930513595166,
+    },
+    {
+      timestamp: '2024-05-05T12:00:00',
+      risk_score: 20.382930513595166,
+    },
+    {
+      timestamp: '2024-05-06T12:00:00',
+      risk_score: 20.382930513595166,
+    },
+    {
+      timestamp: '2024-05-07T12:00:00',
+      risk_score: 20.382930513595166,
+    },
+    {
+      timestamp: '2024-05-08T12:00:00',
+      risk_score: 20.382930513595166,
+    },
+    {
+      timestamp: '2024-05-09T12:00:00',
+      risk_score: 20.382930513595166,
+    },
+    {
+      timestamp: '2024-05-10T12:00:00',
+      risk_score: 20.382930513595166,
+    },
+    {
+      timestamp: '2024-05-11T12:00:00',
+      risk_score: 20.382930513595166,
+    },
+    {
+      timestamp: '2024-05-12T12:00:00',
+      risk_score: 20.382930513595166,
+    },
+    {
+      timestamp: '2024-05-13T12:00:00',
+      risk_score: 20.33610271903323,
+    },
+    {
+      timestamp: '2024-05-14T12:00:00',
+      risk_score: 20.275679758308158,
+    },
+    {
+      timestamp: '2024-05-15T12:00:00',
+      risk_score: 27.01923076923077,
+    },
+    {
+      timestamp: '2024-05-16T12:00:00',
+      risk_score: 24.234375,
+    },
+    {
+      timestamp: '2024-05-17T12:00:00',
+      risk_score: 22.675,
+    },
+    {
+      timestamp: '2024-05-18T12:00:00',
+      risk_score: 23.666666666666668,
+    },
+    {
+      timestamp: '2024-05-19T12:00:00',
+      risk_score: 23.666666666666668,
+    },
+    {
+      timestamp: '2024-05-20T12:00:00',
+      risk_score: 23.666666666666668,
+    },
+    {
+      timestamp: '2024-05-21T12:00:00',
+      risk_score: 23.03409090909091,
+    },
+    {
+      timestamp: '2024-05-22T12:00:00',
+      risk_score: 20.192307692307693,
+    },
+    {
+      timestamp: '2024-05-23T12:00:00',
+      risk_score: 19.375,
+    },
+    {
+      timestamp: '2024-05-24T12:00:00',
+      risk_score: 19.04310344827586,
+    },
+    {
+      timestamp: '2024-05-25T12:00:00',
+      risk_score: 19.04310344827586,
+    },
+    {
+      timestamp: '2024-05-26T12:00:00',
+      risk_score: 19.04310344827586,
+    },
+    {
+      timestamp: '2024-05-27T12:00:00',
+      risk_score: 18.8,
+    },
+    {
+      timestamp: '2024-05-28T12:00:00',
+      risk_score: 18.1328125,
+    },
+    {
+      timestamp: '2024-05-29T12:00:00',
+      risk_score: 17.613636363636363,
+    },
+    {
+      timestamp: '2024-05-30T12:00:00',
+      risk_score: 17.613636363636363,
+    },
+    {
+      timestamp: '2024-05-31T12:00:00',
+      risk_score: 17.613636363636363,
+    },
+    {
+      timestamp: '2024-06-01T12:00:00',
+      risk_score: 17.613636363636363,
+    },
+    {
+      timestamp: '2024-06-02T12:00:00',
+      risk_score: 17.404411764705884,
+    },
+  ],
+  high_risk_areas: [
+    {
+      name: 'Domain Spoofing',
+    },
+    {
+      name: 'Request for Action',
+    },
+    {
+      name: 'Link Manipulation',
+    },
+    {
+      name: 'Malicious Attachments',
+    },
+  ],
+  user_interactions: {
+    links_clicked: 2735,
+    qr_code_scanned: 102,
+    download_attachments: 80,
+    credentials_entered: 300,
+    credentials_submitted: 44,
+    mfa_entered: 44,
+    tests_responded_to: 180,
+    tests_ignored: 5577,
+    reported_to_watchtower: 2021,
+  },
+  risk_categories: {
+    'Low risk': 0.716012084592145,
+    'Moderate risk': 0.22054380664652568,
+    'High risk': 0.054380664652567974,
+    'Severe risk': 0.00906344410876133,
+  },
+};
